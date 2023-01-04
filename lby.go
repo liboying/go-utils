c3dc69f1649739b9ede309c9c2bf854808d8b73b
liboying <46369953+liboying@users.noreply.github.com>
2023-01-04 16:40:32 +0800

    Update and rename cet.go to lby.go

--- lby.go
+++ lby.go
@@ -1,20 +1,14 @@
-package main
+package lby
 
 import (
 	"fmt"
-	"github.com/liboying/go-utils/cet"
 )
 /**:
  * @author: lby
  * @Desc:
  * @create: 2023-01-04 13:42
  */
-func main() {
-	fmt.Println("这是一个测试用的包")
-	Method1()
-}
 
 func Method1() {
 	fmt.Println("这是一个测试方法1")
-	cet.Hello()
 }
